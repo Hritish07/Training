85dee6ff7e3773dea1fbb325ed8b29bc530246bd
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-01 18:53:42 +0530

    if else

---
+++ index3.js
@@ -0,0 +1,17 @@
+console.log("This is tutorial 8")
+let age = 65;
+
+
+if (age!=19)
+{
+    console.log('Age is not 19')
+}
+if(age !==65)
+{
+    console.log('Age is not 65')
+}
+else
+{
+    console.log('Cant match with nay age group')
+}
+//comment
