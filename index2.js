9816a87eec4f59fb17028ad93c3676d08c02b6d6
Hritish Ranjan Jena <91380055+Hritish07@users.noreply.github.com>
2021-10-01 15:02:16 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,33 @@
+const marks = [34,25,12,14,15];
+//const fruits=['orange','apple','banana'];
+//const mixed=['chips',45,[5,8]];
+//const arr=new Array(23,123,21,'orange');
+//console.log(arr)
+//console.log(mixed)
+//console.log(fruits[1])
+
+//arr[0]='hritish'
+//console.log(arr);
+//marks.unshift(1009)
+//console.log(marks)
+//marks.push(76);
+//marks.pop();
+//marks.shift();
+//marks.splice(1,2)
+//marks.reverse()
+
+//let mar=[1,2,3,4]
+//marks = marks.concat(mar)
+//console.log(marks);
+let myObj = {
+    name: 'hritish',
+    age:'23'
+}
+console.log(myObj)
+
+
+
+
+
+    
+
