09737acc4542c6dc35eb1e931b33624733a094db
Hritish Ranjan Jena <91380055+Hritish07@users.noreply.github.com>
2021-10-01 11:23:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var name ='Hritish'
+console.log(name)
+var name='greek07'
+console.log(name)
+const ownername='Spiderman'
+console.log(ownername)
+//ownername='superman' cant change in const.
+//global value
+let city='BBSr'
+{
+    let city='Bhubaneswar'
+    console.log(city)
+    city='cuttak'
+    console.log(city)
+    //local variables are declared
+
+}
+console.log(city)
+const arr1 =[32,1,65,64,07]
+arr1.push(45)
+console.log(arr1)
+let x=16+"eeeN";
+console.log(x)
+let i=7
+console.log(i.toString())
+
+
+
+
+
