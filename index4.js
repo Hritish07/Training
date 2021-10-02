a8fc0f542765e502bf918184caeacdbb3c1ccd40
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-02 10:47:45 +0530

    loops

---
+++ index4.js
@@ -0,0 +1,32 @@
+
+//for (let i=0;i<100;i++)
+//{
+ //   console.log(i);
+//}
+//let j=0;
+//while (j<100) {
+//    console.log(j+1);
+//    j+=1;
+//}
+//let k=110;
+//do{
+//    console.log(k+1)
+//    k +=1
+//}while(k<130)
+//break and continue
+//let arr=[2,3,4,5,6,]
+//arr.forEach(function (elements)
+//{
+//    console.log(elements);
+    
+//});
+//let obj = {
+  //  name:"Hritish",
+    //age:'23',
+    //type:"danger"
+//}
+//for(let key in obj)
+//{
+  //  console.log(`$[key]obj[key] or object is &{obj[key]}`)
+//}
+//console.log("done")
