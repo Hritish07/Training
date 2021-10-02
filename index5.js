65851b1a196fc913616c1f1d02dab7fdf68ef637
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-02 13:09:10 +0530

    function

---
+++ index5.js
@@ -0,0 +1,36 @@
+//function greet(name , thank) {
+  //  console.log(`Happy BirthDay ${name2} wish you many many happy return of the day.be blessed. ${thank}`)
+    
+//}
+//let name='hritish';
+//let name2 ="rohan"
+
+
+//greet(name2,'Thank you');
+
+//const myobj = {
+//    name : 'hritish',
+//    game : function( ) 
+//    {
+//        return "GTA vicecity";
+        
+//    }
+//}
+//console.log(myobj.game());
+
+
+//arr = ['orange','apple','grapes'];
+//arr.array.forEach(function{(elements,array,index)
+ 
+    
+//});
+let name ='hritish'
+var i=234;
+console.log(i);
+function ui(name) {
+    vari=9;
+
+    return 'this is a ${name} ui'
+    
+}
+console.log(ui('hritish'),i);
