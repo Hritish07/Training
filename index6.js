a2bfa6bb33294179c092593ce1a9ef76f1b4e48b
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-02 15:18:15 +0530

    Website Manipulating

---
+++ index6.js
@@ -0,0 +1,32 @@
+let a = window.document;
+ //alert('hello world');
+ 
+// a =prompt('This will destroy your computer.Type your Name')
+//a = confirm('you want to delete this page')
+//let a = window.innerHeight;
+//let a = innerWidth;
+//a= scrollX;
+
+a= location;
+a = window.history;
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
