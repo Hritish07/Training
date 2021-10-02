26e9daa8c86bb6479c9e0e3d8da59a997e5f96b3
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-02 09:40:06 +0530

    if else and switch case

--- index3.js
+++ index3.js
@@ -1,17 +1,43 @@
 console.log("This is tutorial 8")
-let age = 65;
+let age = 38;
+//const hritish=23;
+//const doesDrive = false;
 
+//if(age !=19){
+//console.log('age is not 19')
+//}
+//else(age !=65)
+//{
+  //  console.log('age is not 65')
 
-if (age!=19)
-{
-    console.log('Age is not 19')
+//}
+//if(typeof vari !== 'undefined')
+//{
+//    console.log('vari is defined')
+//}
+//else{
+//    console.log('vari is not defined')
+//}
+//if (doesDrive || age>18)
+//{
+  //  console.log('You can drive')
+//}
+//else{
+   // console.log('You cant Drive')
+//}
+//console.log(age==45? 'age is 45':'age is not 45')
+switch (age) {
+    case 18:
+        console.log('you are 18')
+        break;
+    case 28:
+        console.log('tou are 28')
+        break;
+    
+    case 38:
+        console.log('you are 38')
+        break;
+    default:
+        console.log('You are None of the above')
+        break;
 }
-if(age !==65)
-{
-    console.log('Age is not 65')
-}
-else
-{
-    console.log('Cant match with nay age group')
-}
-//comment
