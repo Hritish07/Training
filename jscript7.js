d5a1867063ed39f097206f1f84f6b35b3a7dcd22
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-02 16:35:02 +0530

    DOM

---
+++ jscript7.js
@@ -0,0 +1,14 @@
+console.log('Wlcome to the website');
+let a = document;
+
+a=document.all;
+a= document.body
+a=document.forms;
+//a= document.forms(0);
+
+
+a = document.links;
+
+console.log(a);
+
+
