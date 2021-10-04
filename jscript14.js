14552ea4a0e34df804bd7ba0ab63c65c80c8098e
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-04 12:54:19 +0530

    Storage

---
+++ jscript14.js
@@ -0,0 +1,15 @@
+console.log('storage system');
+
+let imparr=['apple','orange','grapes'];
+
+
+
+//let name = localStorage.getItem('name2');
+//localStorage.removeItem('name');
+//localstorage.clear() clears th entire localstorage
+
+
+// name = JSON.parse(localStorage.getItem('sessionfruits'));
+
+console.log(imparr);
+//
