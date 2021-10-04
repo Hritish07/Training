3d1e229f29033ea2574a5df907e7894e25229600
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-04 10:12:57 +0530

    More Events

--- jscript12.js
+++ jscript12.js
@@ -1,7 +1,28 @@
 console.log('more java script events');
-let btn= document.getElementById('btn');
+/*let btn= document.getElementById('btn');
 btn.addEventListener('click',func1);
+btn.addEventListener('dblclick',func2);
+
 function func1(e) {
-    console.log('thanks',e);
-    
+    console.log("Thanks",e);
+    e.preventDefault();
 }
+
+function func2(e) {
+    console.log("Thanks adb click",e);
+    e.preventDefault();
+}*/
+//document.querySelector('.no').addEventListener('mouseenter',function () {
+  //  console.log('you entered No');
+    
+//})
+//document.querySelector('.no').addEventListener('mouseleave',function () {
+  //  console.log('you leaved No');
+    
+//})
+document.querySelector('.container').addEventListener('mousemove',function () {
+    document.body.style.backgroundColor=`rgb($)`
+    console.log('you moved No');
+    
+})
+
