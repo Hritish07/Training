aeec6dafd0e5cd35180e06a554561df5738813f8
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-05 15:14:41 +0530

    Mathct0

---
+++ jscript15.js
@@ -0,0 +1,28 @@
+console.log('math function ')
+
+z= Math;
+z= Math.PI;
+z= Math.ceil(5.7) ;
+z= Math.round(5.6);
+z= Math.floor(5.4);
+z= Math.abs(-29);
+z= Math.sqrt(144);
+z= Math.pow(2,3);
+z= Math.min(762,3243,4534,676,67,2);
+
+
+z= 100*Math.random();
+z =50+(100-50)*Math.random();
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(z);
