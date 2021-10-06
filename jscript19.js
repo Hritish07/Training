20e5f8a4e2733c4d35d47f016b6850dfd095f3bb
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-06 13:56:28 +0530

    Prototype

---
+++ jscript19.js
@@ -0,0 +1,26 @@
+let obj = {
+    name: "harry",
+    channel: "Code With Harry",
+    address: "Mars"
+}
+
+function Obj(givenName){
+    this.name = givenName
+}
+
+
+Obj.prototype.getName = function (){
+    return this.name;
+}
+
+Obj.prototype.setName = function (newName){
+  this.name = newName;
+}
+
+
+
+
+
+
+let obj2 = new Obj("Hritish");
+console.log(obj2);
