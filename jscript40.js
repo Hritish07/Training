19ee44a70b7584b7065dbe7a363a65b155e965b7
Hritish Ranjan Jena <hritishranjanjena@gmail.com>
2021-10-11 16:11:18 +0530

    Symbols

---
+++ jscript40.js
@@ -0,0 +1,15 @@
+console.log('Symbol in Javascript');
+
+
+const sym1=Symbol('my identifier');
+const sym2=Symbol('my identifier');
+
+
+console.log(sym1 === sym2);
+
+
+const a ="this is";
+const b ="this is";
+
+console.log(a === b);
+console.log(null === null );
